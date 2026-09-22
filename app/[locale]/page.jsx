dd10a7c00cd40915e28563e08399b99b29df74ee
faultmind <madi@faultmind.com>
2026-09-22 10:02:48 +0300

    Implement redirect to '/en' in RootPage
    Added RootPage function to redirect to '/en'.

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
 import { useRouter, usePathname } from "../../i18n/routing";
 import { supabase } from "../../lib/supabaseClient";
+import { redirect } from 'next/navigation';
 
 export default function Page() {
   const tHeader = useTranslations("Header");
@@ -390,4 +391,7 @@ export default function Page() {
       </div>
     </main>
   );
+}
+export default function RootPage() {
+  redirect('/en');
 }
